46ef62ea21952c0421873f60e92af54664ba8705
bovaqulov <bovaqulov@gmail.com>
2025-11-21 12:05:09 +0500

    only JSON

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ REST_FRAMEWORK = {
     # Renderer va Parser sozlamalari
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
