f461780ac582f90b38c0afe22593c83997db2e20
bovaqulov <bovaqulov@gmail.com>
2025-11-28 15:31:05 +0500

    url databse s

--- config/settings.py
+++ config/settings.py
@@ -149,11 +149,10 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    BASE_DIR / "staticfiles",  # agar sizda qo‘shimcha static papkalar bo‘lsa
 ]
 
 # Default primary key field type
