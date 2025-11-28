fdeccb8ddfd57bcd641c4c9d77acbc13f06ba038
bovaqulov <bovaqulov@gmail.com>
2025-11-28 11:53:37 +0500

    CORS swagger 3

--- config/settings.py
+++ config/settings.py
@@ -181,7 +181,7 @@ SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
         'Token': {
             'type': 'apiKey',
-            'name': 'Authorization: Token',
+            'name': 'Authorization',
             'in': 'header'
         }
     },
