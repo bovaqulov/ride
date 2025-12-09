b38da42fe70dad90bb7e673cb12db60a00311a15
bovaqulov <bovaqulov@gmail.com>
2025-12-09 10:26:00 +0500

    salom 6

--- bot_app/signals/order_signals.py
+++ bot_app/signals/order_signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
-from ..models import Order, TravelStatus
+from ..models import Order, TravelStatus, Driver
 from ..tasks.travel_tasks import notify_driver_bot, notify_passenger_bot
 
 
@@ -11,7 +11,14 @@ def update_order(sender, instance: Order, **kwargs):
     if instance.driver:
         if instance.status == TravelStatus.CREATED:
             instance.status = TravelStatus.ASSIGNED
-        notify_passenger_bot.delay(instance.pk)
+            try:
+                driver = Driver.objects.get(pk=instance.driver.pk)
+                driver.amount -= instance.content_object.price * 0.05
+                driver.save()
+            except Exception as e:
+                pass
+
+            notify_passenger_bot.delay(instance.pk)
 
     if instance.driver and instance.status == TravelStatus.ENDED:
         notify_passenger_bot.delay(instance.pk)
