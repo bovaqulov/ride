aa7fc21dade9ea7033adf3a60a2fb8ca2c8ea459
bovaqulov <bovaqulov@gmail.com>
2025-12-09 10:33:22 +0500

    salom 6

--- bot_app/signals/order_signals.py
+++ bot_app/signals/order_signals.py
@@ -8,17 +8,19 @@ from ..tasks.travel_tasks import notify_driver_bot, notify_passenger_bot
 @receiver(pre_save, sender=Order)
 def update_order(sender, instance: Order, **kwargs):
 
-    if instance.driver:
-        if instance.status == TravelStatus.CREATED:
-            instance.status = TravelStatus.ASSIGNED
-            try:
-                driver = Driver.objects.get(pk=instance.driver.pk)
-                driver.amount -= instance.content_object.price * 0.05
-                driver.save()
-            except Exception as e:
-                pass
+    if instance.driver and (instance.status == TravelStatus.CREATED or instance.status == TravelStatus.ASSIGNED):
 
-            notify_passenger_bot.delay(instance.pk)
+        instance.status = TravelStatus.ASSIGNED
+        try:
+            driver = Driver.objects.get(pk=instance.driver.pk)
+            driver.amount -= instance.content_object.price * 0.05
+            driver.save()
+        except Exception as e:
+            pass
+
+        notify_passenger_bot.delay(instance.pk)
+
+
 
     if instance.driver and instance.status == TravelStatus.ENDED:
         notify_passenger_bot.delay(instance.pk)
