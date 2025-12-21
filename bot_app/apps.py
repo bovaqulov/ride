0ee28f24b5ebea015c43172271197dcbf068f5ab
bovaqulov <bovaqulov@gmail.com>
2025-12-21 16:30:27 +0500

    add phone

--- bot_app/apps.py
+++ bot_app/apps.py
@@ -6,6 +6,7 @@ from django.apps import AppConfig
 class BotAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot_app'
+    verbose_name = 'Bot management'
 
     def ready(self):
         import bot_app.signals
